2877f158fe80cd52506bcbef6d44b213ba6b1614
Georgies Pharmacy <admin@georgiespharmacy.com>
2025-08-06 07:39:51 -0400

    Add quick-login to middleware exceptions

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,7 +16,8 @@ export function middleware(request: NextRequest) {
     pathname === '/reset-password' ||
     pathname === '/signout' ||
     pathname === '/contact' ||
-    pathname === '/auth-test'
+    pathname === '/auth-test' ||
+    pathname === '/quick-login'
   ) {
     return NextResponse.next()
   }
